e325d2c32c5ca12e8f3aaef0fd0dd0c1f2718455
Гавриш Олександр <sashawwe21@gmail.com>
2024-11-08 20:32:27 +0200

    add blog

--- src/app/components/terms-of-use/terms-of-use.component.ts
+++ src/app/components/terms-of-use/terms-of-use.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { FormService } from '../../services/form.service';
 
 @Component({
   selector: 'app-terms-of-use',
@@ -6,10 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './terms-of-use.component.scss'
 })
 export class TermsOfUseComponent {
+
+  constructor(private renderer: Renderer2, private formService: FormService) { }
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   }
+
+  isContactOpen = false;
+  contactName
+  contactPhone
+  successForm = false
+
+  openContact() {
+    this.isContactOpen = true;
+    this.renderer.addClass(document.body, 'no-scroll');
+  }
+
+  closeContact() {
+    this.isContactOpen = false;
+    // Видаляємо клас, щоб дозволити скрол після закриття форми
+    this.renderer.removeClass(document.body, 'no-scroll');
+  }
+
+  sendContact(){
+    this.formService.sendFormData(this.contactName, this.contactPhone).subscribe((response) => {
+      this.successForm = true
+      setTimeout(() => {
+        this.closeContact()
+        this.successForm = false
+      }, 30000)
+    });
+ }
 }
