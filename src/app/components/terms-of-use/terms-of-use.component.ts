abd2359bfe621c5e591c47ba9e917395e28707ac
Гавриш Олександр <sashawwe21@gmail.com>
2024-11-09 16:54:14 +0200

    fix footer for mobile

--- src/app/components/terms-of-use/terms-of-use.component.ts
+++ src/app/components/terms-of-use/terms-of-use.component.ts
@@ -8,37 +8,11 @@ import { FormService } from '../../services/form.service';
 })
 export class TermsOfUseComponent {
 
-  constructor(private renderer: Renderer2, private formService: FormService) { }
+  constructor(private renderer: Renderer2) { }
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
   }
-
-  isContactOpen = false;
-  contactName
-  contactPhone
-  successForm = false
-
-  openContact() {
-    this.isContactOpen = true;
-    this.renderer.addClass(document.body, 'no-scroll');
-  }
-
-  closeContact() {
-    this.isContactOpen = false;
-    // Видаляємо клас, щоб дозволити скрол після закриття форми
-    this.renderer.removeClass(document.body, 'no-scroll');
-  }
-
-  sendContact(){
-    this.formService.sendFormData(this.contactName, this.contactPhone).subscribe((response) => {
-      this.successForm = true
-      setTimeout(() => {
-        this.closeContact()
-        this.successForm = false
-      }, 30000)
-    });
- }
 }
